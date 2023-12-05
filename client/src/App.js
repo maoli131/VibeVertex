cc5ea56ebf4b638e00f104febb5a2c8430a05e29
maoli131 <personal@li-mao.me>
2023-12-05 03:15:44 -0800

    update: fonts, colors and design for the main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
 import './App.css';
 
 function App() {
-  const sectionStyle = (backgroundColor) => ({
-    height: '40vh', // 1/3 of the viewport height
-    display: 'flex',
-    justifyContent: 'center', // Center horizontally
-    alignItems: 'center', // Center vertically
-    backgroundColor, // Background color passed as an argument
-    color: 'white', // Text color
-    fontSize: '2em' // Larger text
-  });
+  const sectionStyle = (colorTheme) => {
+    const isLight = colorTheme === 'light';
+    const lightColor = '#FEF2F4';
+    const darkColor = '#FCC8D1';
+    return {
+      height: '40vh', // percentage of view
+      display: 'flex',
+      justifyContent: 'center', // Center horizontally
+      alignItems: 'center', // Center vertically
+      backgroundColor: isLight ? lightColor : darkColor, // Background color
+      color: isLight ? darkColor : lightColor, // Text color
+      fontSize: '6em', // Larger text
+      fontWeight: 200,
+    }
+  };
 
   return (
     <div>
-      <div style={sectionStyle('#F1CEC8')}>
-        Truth
+      <div style={sectionStyle('dark')}>
+        真心话
       </div>
-      <div style={sectionStyle('#CC978C')}>
-        Dare
+      <div style={sectionStyle('light')}>
+        大冒险
       </div>
-      <div style={sectionStyle('#D4C6BA')}>
-        Game
+      <div style={sectionStyle('dark')}>
+        小游戏
       </div>
     </div>
   );
