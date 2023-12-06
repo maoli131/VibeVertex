5b851168b7b11a3d2383dc367ceb58bc43d03435
maoli131 <personal@li-mao.me>
2023-12-05 22:09:35 -0800

    [add] react frontend and node backend integration template

--- client/src/components/Truth.js
+++ client/src/components/Truth.js
@@ -1,8 +1,24 @@
-// Truth.js
+import React, { useState, useEffect } from 'react';
+
 function Truth() {
+
+	const [data, setData] = useState(null);
+
+	useEffect(() => {
+		fetch('http://localhost:3001/api/truth')
+			.then(response => response.json())
+			.then(data => {
+				setData(data);
+			})
+			.catch(error => {
+				console.error('Error fetching data: ', error);
+			});
+	}, []);
+
 	return (
 		<div>
 			<h1>真心话</h1>
+			{data && <p>{data.message}</p>}
 		</div>
 	);
 }
--- server.js
+++ server.js
@@ -3,11 +3,22 @@
  */
 
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
+
 app.get('/', (req, res) => {
     res.send('Hello from the server');
+});
+
+app.get('/api/truth', (req, res) => {
+    const data = {
+        message: 'This is truth from the backend.'
+    };
+    res.json(data);
 });
 
 app.listen(PORT, () => {
