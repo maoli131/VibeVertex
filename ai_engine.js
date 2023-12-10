f75edaf569bacf146c0289d5a41399490a5c2567
maoli131 <personal@li-mao.me>
2023-12-10 15:26:36 -0800

    update: minor syntax update in ai engine

--- ai_engine.js
+++ ai_engine.js
@@ -16,7 +16,7 @@ async function genTruthQuestions(num) {
     response_format: { "type": "json_object" },
     messages: [
       { role: "system", content: "你是一个人工智能派对助手，你是一群很熟悉、彼此非常亲密的朋友的派对助手。你将用JSON的格式输出答案，JSON格式如下: {title: '真心话', messages: [问题，问题，问题，...]}。保持title部分一定为:'真心话'，而不是别的。" },
-      { role: "user", content: "帮我想一些真心话的题目，要求题目大胆、劲爆、有趣、好玩，部分可以涉及成人话题，最好能增进朋友们相互了解。给我" + num + "个这样的题目。" }
+      { role: "user", content: `帮我想一些真心话的题目，要求题目大胆、劲爆、有趣、好玩，部分可以涉及成人话题，最好能增进朋友们相互了解。给我${num}个这样的题目。` }
     ],
     model: "gpt-4-1106-preview",
   });
@@ -28,7 +28,7 @@ async function genDareQuestions(num) {
     response_format: { "type": "json_object" },
     messages: [
       { role: "system", content: "你是一个人工智能派对助手，你是一群很熟悉、彼此非常亲密的朋友的派对助手。你将用JSON的格式输出答案，JSON格式如下: {title: '大冒险', messages: [问题，问题，问题，...]}。保持title部分一定为:'大冒险'，而不是别的。" },
-      { role: "user", content: "帮我想一些大冒险的题目，要求题目大胆、劲爆、有趣、好玩。给我" + num + "个这样的题目。" }
+      { role: "user", content: `帮我想一些大冒险的题目，要求题目大胆、劲爆、有趣、好玩。给我${num}个这样的题目。` }
     ],
     model: "gpt-4-1106-preview",
   });
@@ -40,7 +40,7 @@ async function genGameQuestions(num) {
     response_format: { "type": "json_object" },
     messages: [
       { role: "system", content: "你是一个人工智能派对助手，你是一群很熟悉、彼此非常亲密的朋友的派对助手。你将用JSON的格式输出答案，JSON格式如下: {title: '小游戏', messages: [问题，问题，问题，...]}。保持title部分一定为:'小游戏'，而不是别的。" },
-      { role: "user", content: "帮我想一些有趣的酒桌游戏，适合好朋友们一起喝酒玩。给我" + num + "个这样的游戏。" }
+      { role: "user", content: `帮我想一些有趣的酒桌游戏，适合好朋友们一起喝酒玩。给我${num}个这样的游戏。` }
     ],
     model: "gpt-4-1106-preview",
   });
